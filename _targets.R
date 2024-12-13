9a25937531579c83971f76f6c15a82bd0a08127a
Michael Flynn <flynn.michael.e@gmail.com>
2024-12-13 13:12:14 -0600

    Update gitignore to include xlsx files for data that were excluded from repo.

--- _targets.R
+++ _targets.R
@@ -32,7 +32,7 @@ tar_option_set(
   packages = c("tibble", "tidyverse", "data.table", "brms", "sf", "tidybayes", "modelsummary", "cmdstanr",
                "marginaleffects", "flynnprojects", "viridis", "glue", "here", "kableExtra",
                "purrr", "furrr", "svglite", "tarchetypes", "quarto", "future", "ggdist", "tinytex",
-               "bayesplot", "patchwork", "glue", "here", "scales", "sysfonts"), # packages that your targets need to run
+               "bayesplot", "patchwork", "glue", "here", "scales", "sysfonts", "showtext"), # packages that your targets need to run
   format = "rds" # default storage format
   # Set other options as needed.
 )
